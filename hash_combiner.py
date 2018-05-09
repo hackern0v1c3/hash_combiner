628a931c3e34255cf431a16b552f20f6a99b9fb9
hackernovice <kev@hackernovice.com>
2018-05-09 14:39:56 -0400

    Initial script commit

---
+++ hash_combiner.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+import os
+import sys
+
+def print_usage():
+	print "USAGE: "+sys.argv[0]+" {path to file containing Username:Hash} {path to file containing Hash:Password}"
+	print "Example: "+sys.argv[0]+" users_with_hashes.txt cracked_hashes.txt"
+
+def combineInput():
+  userHashes = {}
+  crackedPasswords = {}
+
+  with open(sys.argv[1], 'r') as f:
+    for line in f:
+      splitLine = line.split(":")
+      userHashes[splitLine[0]] = splitLine[1].rstrip()
+
+  with open(sys.argv[2], 'r') as f:
+    for line in f:
+      splitLine = line.split(":")
+      crackedPasswords[splitLine[0]] = splitLine[1].rstrip()
+
+  for user in userHashes:
+    for passwordHash in crackedPasswords:
+      if userHashes[user] == passwordHash:
+        print user, ":", crackedPasswords[passwordHash]
+
+#MAIN
+if len(sys.argv) != 3:
+	print_usage()
+	sys.exit()
+
+if os.path.isfile(sys.argv[1]) and os.path.isfile(sys.argv[2]):
+	combineInput()
+else:
+	print "Invalid file"
